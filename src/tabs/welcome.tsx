6f52916fcc1aa735149bd16eae28ef65663873bf
Paweł Gawlik <pgawlik215@gmail.com>
2024-11-08 19:56:09 +0100

    feat: add welcome tab

---
+++ src/tabs/welcome.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Welcome() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      <h2>Welcome Tab</h2>
+
+      <p>This tab is Welcome tab.</p>
+    </div>
+  );
+}
+
+export default Welcome;
