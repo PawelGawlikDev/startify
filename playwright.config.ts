60ced64fe70e61c104afe8fc1a9197f1f699c3d3
Paweł Gawlik <pgawlik215@gmail.com>
2025-01-21 21:39:07 +0100

    fix: fix tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   testMatch: [/.*\.spec\.(ts)/],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  retries: process.env.CI ? 2 : 1,
+  workers: process.env.CI ? 1 : 1,
   reporter: process.env.CI ? [["github"], ["list"]] : "html",
 
   projects: [
