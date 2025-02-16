6078b0aaf2d0c389051bd27ce54ffa727ea71dd5
Paweł Gawlik <pgawlik215@gmail.com>
2025-02-16 12:29:04 +0100

    feat: introduce storybook

--- src/utils/backgroundMap.ts
+++ src/utils/backgroundMap.ts
@@ -1,10 +1,8 @@
 const BgTypes = {
   beams: () => import("~components/backgrounds/BackgroundRain"),
-  lines: () => import("~components/backgrounds/BackgroundLines"),
   gradient: () => import("~components/backgrounds/GradientBackground"),
   image: () => import("~components/backgrounds/ImageBackground"),
   aurora: () => import("~components/backgrounds/AuroraBackground"),
-  boxes: () => import("~components/backgrounds/BackgroundBoxes"),
   snakes: () => import("~components/backgrounds/BackgroundSnakes"),
   random: () => import("~components/backgrounds/RandomBackground")
 };
