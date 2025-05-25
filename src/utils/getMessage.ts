3715f2946fcaae695cee2e764091ed4bf38a5ca4
Paweł Gawlik <pgawlik215@gmail.com>
2025-05-25 19:01:16 +0200

    fix: optimalize workflow

--- src/utils/getMessage.ts
+++ src/utils/getMessage.ts
@@ -1,4 +1,6 @@
+import en from "/_locales/en/messages.json?url";
+
 export const getMessage = (key: string): string => {
   // @ts-expect-error: Suppress TypeScript error for dynamic message key
-  return browser.i18n.getMessage(key);
+  return browser.i18n.getMessage(key) ?? en[key].message;
 };
