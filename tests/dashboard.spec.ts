eb714074b68f83e06f60fb9178e78c82c34cfa83
Paweł Gawlik <94375876+PawelGawlikDev@users.noreply.github.com>
2025-05-25 18:49:17 +0200

    test: introduce tests (#82)

---
+++ tests/dashboard.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from "./fixtures/fixtures";
+
+test.describe("Dashboard tests", () => {
+  test("Digital timer visible", async ({ page, extensionId, dashboard }) => {
+    await dashboard.goToExtensionPage(extensionId, dashboard.newTab);
+
+    const digitalTime = page.getByTestId("DigitalTime");
+
+    await expect(digitalTime).toBeVisible();
+  });
+
+  test("Search Input visible", async ({ page, extensionId, dashboard }) => {
+    await dashboard.goToExtensionPage(extensionId, dashboard.newTab);
+
+    const digitalTime = page.getByTestId("SearchInput");
+
+    await expect(digitalTime).toBeVisible();
+  });
+
+  test("Add Quick Link button visible", async ({
+    page,
+    extensionId,
+    dashboard
+  }) => {
+    await dashboard.goToExtensionPage(extensionId, dashboard.newTab);
+
+    const digitalTime = page.getByTestId("AddQuickLink");
+
+    await expect(digitalTime).toBeVisible();
+  });
+
+  test("Quick Link grid visible", async ({ page, extensionId, dashboard }) => {
+    await dashboard.goToExtensionPage(extensionId, dashboard.newTab);
+
+    const digitalTime = page.getByTestId("QuickLinkGrid");
+
+    await expect(digitalTime).toBeVisible();
+  });
+});
